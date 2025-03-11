c268ddb5bf860a217f4239c97582a361737f07f5
Tao Zhihua, CIS-83(deloitte) <zhihua.tao@partner.lingyue-digital.com>
2025-03-11 11:39:56 +0800

    feat: 初始化 ThreeC-JVM项目

---
+++ src/main/java/com/threec/ThreeCApplication.java
@@ -0,0 +1,11 @@
+package com.threec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeCApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThreeCApplication.class, args);
+    }
+}
